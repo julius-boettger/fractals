58a51e8036e57f77a348f8f6a52a87574ec9718b
julius-boettger <julius.btg@proton.me>
2025-03-17 09:36:21 +0100

    feat: set up project structure

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default"; # can run on all systems
+  };
+
+  outputs = { self, nixpkgs, systems, ... }:
+  let
+    eachSystem = fn: nixpkgs.lib.genAttrs (import systems) (system: fn system (import nixpkgs {
+      inherit system;
+    }));
+  in
+  {
+    packages = eachSystem (system: pkgs: rec {
+      default = fractals;
+      fractals = pkgs.rustPlatform.buildRustPackage {
+        name = "fractals";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        #nativeBuildInputs = with pkgs; [
+        #  pkg-config # to find buildInputs
+        #];
+        #buildInputs = with pkgs; [];
+      };
+    });
+
+    devShells = eachSystem (system: pkgs: {
+      default = let
+        inherit (self.packages.${system}.default) nativeBuildInputs buildInputs;
+      in pkgs.mkShell {
+        buildInputs = buildInputs;
+        nativeBuildInputs = nativeBuildInputs ++ (with pkgs; [
+          cargo
+          cargo-edit # provides `cargo upgrade` for dependencies
+        ]);
+        # fix rust-analyzer in vscode
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+  };
+}
