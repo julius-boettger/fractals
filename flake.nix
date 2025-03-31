4449076ae3f262c3cc7df5198ea2e54f9b168955
julius-boettger <julius.btg@proton.me>
2025-03-31 08:22:06 +0200

    nix: add package

--- flake.nix
+++ flake.nix
@@ -9,18 +9,37 @@
     eachSystem = fn: nixpkgs.lib.genAttrs (import systems) (system: fn system (import nixpkgs {
       inherit system;
     }));
+
+    runtimeDeps = pkgs: with pkgs; [
+      # for winit (https://github.com/rust-windowing/winit/issues/3244)
+      wayland
+      libxkbcommon
+      # for the wgpu vulkan backend
+      vulkan-loader
+    ];
   in
   {
+    packages = eachSystem (system: pkgs: rec {
+      default = fractals;
+      fractals = pkgs.rustPlatform.buildRustPackage rec {
+        name = "fractals";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        buildInputs = runtimeDeps pkgs;
+
+        # make sure runtime dependencies get picked up
+        # inspired by https://github.com/NixOS/nixpkgs/blob/52faf482a3889b7619003c0daec593a1912fddc1/pkgs/by-name/al/alacritty/package.nix
+        dontPatchELF = true;
+        postInstall = ''patchelf --add-rpath "${pkgs.lib.makeLibraryPath buildInputs}" $out/bin/${name}'';
+      };
+    });
+
     devShells = eachSystem (system: pkgs: {
       default = pkgs.mkShell {
-        # runtime dependencies
-        LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-          # for winit (https://github.com/rust-windowing/winit/issues/3244)
-          wayland
-          libxkbcommon
-          # for the wgpu vulkan backend
-          vulkan-loader
-        ];
+        # make sure runtime dependencies get picked up
+        # buildInputs doesnt work, see https://github.com/rust-windowing/winit/issues/3244
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (runtimeDeps pkgs);
 
         nativeBuildInputs = with pkgs; [
           cargo
