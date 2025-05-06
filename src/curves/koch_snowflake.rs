dead40932a761498de50a41345eb931b70a4dd54
julius-boettger <julius.btg@proton.me>
2025-05-06 11:09:15 +0200

    refactor: add CurveComponent as intermediate representation

--- src/curves/koch_snowflake.rs
+++ src/curves/koch_snowflake.rs
@@ -1,11 +1,30 @@
 use rayon::prelude::*;
 
-use crate::curves::Curve;
+use crate::curves::{Curve, CurveComponent};
 use crate::renderer::vertex::{Vertex, VertexFormat, vec2::Vec2};
 
 /// https://en.wikipedia.org/wiki/Koch_snowflake
 pub struct KochSnowflake {
-    data: Vec<Vec<Vertex>>
+    data: Vec<(Vec<Line>, Option<Vec<Vertex>>)>
+}
+
+pub struct Line {
+    a: Vertex,
+    b: Vertex,
+    /// direction to point next extension of this line in  
+    top: Vec2,
+}
+
+impl Line {
+    fn new(a: Vertex, b: Vertex, top: Vec2) -> Self {
+        Self { a, b, top }
+    }
+}
+
+impl CurveComponent for Line {
+    fn vertices(&self) -> Vec<Vertex> {
+        vec![self.a, self.b]
+    }
 }
 
 impl KochSnowflake {
@@ -14,71 +33,80 @@ impl KochSnowflake {
     const HEIGHT_DIVISOR: f32 = 4.;
 }
 
-impl Curve for KochSnowflake {
+impl Curve<Line> for KochSnowflake {
     fn new() -> Self {
         Self {
-            data: vec![vec![
-                Vertex::new(Vec2::new(-0.75, -0.45), 0),
-                Vertex::new(Vec2::new( 0.75, -0.45), 0),
+            data: vec![(
+                // actual lines
+                vec![
+                    Vertex::new(Vec2::new(-0.75, -0.45), 0),
+                    Vertex::new(Vec2::new( 0.75, -0.45), 0),
 
-                Vertex::new(Vec2::new(-0.75, -0.45), 0),
-                Vertex::new(Vec2::new( 0.00,  0.75), 0),
+                    Vertex::new(Vec2::new(-0.75, -0.45), 0),
+                    Vertex::new(Vec2::new( 0.00,  0.75), 0),
 
-                Vertex::new(Vec2::new( 0.75, -0.45), 0),
-                Vertex::new(Vec2::new( 0.00,  0.75), 0),
-            ]],
+                    Vertex::new(Vec2::new( 0.75, -0.45), 0),
+                    Vertex::new(Vec2::new( 0.00,  0.75), 0),
+                ]
+                // add top
+                .chunks(2).map(|line| {
+                    let a = line[0];
+                    let b = line[1];
+                    let a_to_b = b.position - a.position;
+                    Line::new(a, b, a_to_b.away_orthogonal(a.position))
+                }).collect(),
+                None
+            )],
         }
     }
 
     fn vertex_format() -> VertexFormat { VertexFormat::Lines }
     
-    fn data(&self) -> &Vec<Vec<Vertex>> {
-        &self.data
-    }
+    fn     data(&    self) -> &    Vec<(Vec<Line>, Option<Vec<Vertex>>)> { &    self.data }
+    fn mut_data(&mut self) -> &mut Vec<(Vec<Line>, Option<Vec<Vertex>>)> { &mut self.data }
+    
+    fn next_iteration(&self, last_lines: &Vec<Line>, iteration: u32) -> Vec<Line> {
+        last_lines.par_iter().map(|line| {
+            let (a, b) = (line.a.position, line.b.position);
+            let (a_iter, b_iter) = (line.a.iteration, line.b.iteration);
 
-    fn next_iteration(&mut self) {
-        let last_vertices = self.data.last().unwrap();
+            let a_to_b = b - a;
 
-        let iteration = self.data.len().try_into().unwrap();
-        if iteration >= 10 {
-            log::debug!("computing iteration {}", iteration);
-        }
+            let third_a = a + ( a_to_b / Self::WIDTH_DIVISOR);
+            let third_b = b + (-a_to_b / Self::WIDTH_DIVISOR);
 
-        let vertices = last_vertices
-            .par_chunks(2)
-            .map(|line| {
-                let (a, b) = (line[0].position, line[1].position);
-                let (a_iter, b_iter) = (line[0].iteration, line[1].iteration);
+            let top = {
+                // with reduced length
+                let away_orthogonal = a_to_b.away_orthogonal(a) / Self::HEIGHT_DIVISOR;
 
-                let a_to_b = b - a;
+                a + (a_to_b / 2.) + away_orthogonal
+            };
 
-                let third_a = a + ( a_to_b / Self::WIDTH_DIVISOR);
-                let third_b = b + (-a_to_b / Self::WIDTH_DIVISOR);
-
-                let top = {
-                    // with reduced length
-                    let away_orthogonal = a_to_b.away_orthogonal(a) / Self::HEIGHT_DIVISOR;
-
-                    a + (a_to_b / 2.) + away_orthogonal
-                };
-
-                [
+            [
+                Line::new(
                     Vertex::new(a,       a_iter),
                     Vertex::new(third_a, a_iter),
+                    Vec2::new(0., 0.),
+                ),
 
+                Line::new(
                     Vertex::new(third_a, a_iter),
                     Vertex::new(top,     iteration),
+                    Vec2::new(0., 0.),
+                ),
 
+                Line::new(
                     Vertex::new(top,     iteration),
                     Vertex::new(third_b, b_iter),
+                    Vec2::new(0., 0.),
+                ),
 
+                Line::new(
                     Vertex::new(third_b, b_iter),
                     Vertex::new(b,       b_iter),
-                ]
-            })
-            .flatten()
-            .collect();
-
-        self.data.push(vertices);
+                    Vec2::new(0., 0.),
+                ),
+            ]
+        }).flatten().collect()
     }
 }
--- src/curves/mod.rs
+++ src/curves/mod.rs
@@ -3,17 +3,43 @@ pub mod koch_snowflake;
 use crate::renderer::vertex::{Vertex, VertexFormat};
 
 /// https://en.wikipedia.org/wiki/Fractal_curve
-pub trait Curve {
+pub trait Curve<Component: CurveComponent> {
     fn new() -> Self;
     fn vertex_format() -> VertexFormat;
-    fn data(&self) -> &Vec<Vec<Vertex>>;
-    fn next_iteration(&mut self);
+    fn next_iteration(&self, last_components: &Vec<Component>, iteration: u32) -> Vec<Component>;
+
+    /// one element for each iteration, where each element consists of
+    /// the iterations components, and optionally their computed vertices
+    fn     data(&    self) -> &    Vec<(Vec<Component>, Option<Vec<Vertex>>)>;
+    fn mut_data(&mut self) -> &mut Vec<(Vec<Component>, Option<Vec<Vertex>>)>;
 
     /// iteration 0 meaning initial state
-    fn vertices(&mut self, iteration: usize) -> &Vec<Vertex> {
-        while self.data().len() <= iteration {
-            self.next_iteration();
+    fn vertices<'a>(&'a mut self, iteration: usize) -> &'a Vec<Vertex>
+        where Component: 'a
+    {
+        // compute fractal iterations (if not done already)
+        for current_iteration in self.data().len() ..= iteration {
+            if current_iteration >= 10 {
+                log::debug!("computing iteration {}", current_iteration);
+            }
+
+            let next_data = self.next_iteration(
+                &self.data()[current_iteration - 1].0,
+                current_iteration.try_into().unwrap()
+            );
+
+            self.mut_data().push((next_data, None));
         }
+
+        // map components of iteration to vertices (if not done already)
+        if self.data()[iteration].1 == None {
+            let vertices = self.data()[iteration].0.iter()
+                .map(|c| c.vertices())
+                .flatten()
+                .collect();
+
+            self.mut_data()[iteration].1 = Some(vertices);
+        };
 
         let fractal_name = std::any::type_name::<Self>()
             .rsplit_once("::")
@@ -21,6 +47,10 @@ pub trait Curve {
             .1;
         log::info!("computed iteration {} of {}", iteration + 1, fractal_name);
 
-        &self.data()[iteration]
+        &self.data()[iteration].1.as_ref().unwrap()
     }
+}
+
+pub trait CurveComponent {
+    fn vertices(&self) -> Vec<Vertex>;
 }
